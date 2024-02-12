e218246f4f5354bb51d09e056feadb9e2ce87637
mdvertola <mdvertola@gmail.com>
2024-02-11 20:14:03 -0500

    fixing stack name

--- aws/bin/aws.ts
+++ aws/bin/aws.ts
@@ -3,4 +3,4 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsStack } from '../lib/aws-stack';
  
 const app = new cdk.App();
-new AwsStack(app, 'LandingPageStack',{domainName: 'beaumontlangley.com'});
+new AwsStack(app, 'BAndLLandingPageStack',{domainName: 'beaumontlangley.com'});
